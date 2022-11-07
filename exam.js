d52dca74a56f07f434c6090964607b8bd2ce4ec4
czafersyuz <czafersyuz@gmail.com>
2022-11-07 12:45:00 +0300

    loops

---
+++ exam.js
@@ -0,0 +1,19 @@
+//10 kere merhaba yazdır
+// for(let i=1; i<=10; i++){
+//     console.log("merhaba")
+// }
+
+//random sayı üretme
+// for (let i = 1; i < 10; i++) {
+//     const randomSayı = ;
+    
+
+// }
+
+
+//do while
+
+do{
+    console.log("merhaba-",i)
+    i++
+}while(i<6)
