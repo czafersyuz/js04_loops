cea2ccb3bd9e3154772cd17865390c672a764030
czafersyuz <czafersyuz@gmail.com>
2022-11-08 18:21:34 +0300

    tutulan sayı çöz

--- exam.js
+++ exam.js
@@ -13,7 +13,43 @@
 
 //do while
 
-do{
-    console.log("merhaba-",i)
-    i++
-}while(i<6)
+// let i=0
+
+// do{
+//     console.log("merhaba-",i)
+//     i++
+// }while(i<6)
+
+// do while not
+
+// let not;
+// do{
+//    not=prompt("1 ile 100 arası bir sayı giriniz!")
+// if (not<0||not>100) {
+//     alert("girdiginiz not 1-100 arasında olmalıdır")
+    
+// }
+// }while(not<0||not>100)
+// console.log("girdiiniz not", not)
+
+
+//while not örnegi
+
+// let not =prompt("notunuzu giriniz (1-100)")
+
+// while (not<0||not>100) {
+//     console.log("lütfen 1 ile 100 arası bir sayı giriniz")
+//     not=prompt("notunuzu giriniz")
+// }
+// alert(`girdiiniz not, ${not}`)
+
+
+
+//tahmin oyunu
+
+let hak=5
+do {
+    let tutulanSayı=Math.floor(math.random()*100+1)
+    console.log(tutulanSayı);
+} while (condition);
+
